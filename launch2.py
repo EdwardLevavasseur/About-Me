77c82926fd5f988708a9ad712845b1487490e174
EdwardLevavasseur <65655204+EdwardLevavasseur@users.noreply.github.com>
2021-05-30 16:36:10 +0200

    Delete launch2.py

--- launch2.py
+++
@@ -1,8 +0,0 @@
-import gradio
-
-def hello(inp):
-  return "Hello {}!".format(inp)
-
-io = gradio.Interface(fn=hello, inputs='text', outputs='text', title='Hello World', 
-    description='The simplest Hosted interface.')  
-io.launch()
